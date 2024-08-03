0bd279577aa319b9f6ee11b8931f97699286ee14
jannet18 <janetngei18@gmail.com>
2024-08-03 14:52:37 +0300

    mpesa popup functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,8 +55,12 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+  # config.hosts.clear
+ 
+  config.hosts << "1bf4-41-80-112-182.ngrok-free.app"
+  config.hosts << "1bf4-41-80-112-182.ngrok-free.app"
 
-
+    config.hosts << /[a-z0-9\-\.]+\.ngrok\.io/
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :mpesas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "access_token", to: "mpesas#get_access_token"
+  post "stkpush", to: "mpesas#stkpush"
+  post 'polling_payment', to: 'mpesas#polling_payment'
+  post "callback_url", to: "mpesas#callback_url"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :volunteers, only: [:create]
+  resources :programs do
+    resources :categories, only: [:index, :show]
+  end
+  resources :students, only: [:index, :create]
+  resources :products, only: [:index, :show]
 end
